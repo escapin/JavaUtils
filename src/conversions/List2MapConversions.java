00efd749c50c8618496265a0caaaeb9210305518
Enrico Scapin <enrico@scapin.io>
2019-04-18 22:36:29 +0200

    Improvements in comparators and testing the descending order

--- src/conversions/List2MapConversions.java
+++ src/conversions/List2MapConversions.java
@@ -31,7 +31,8 @@ public class List2MapConversions {
 
 
 	// Generic function to convert a List of Map.Entry<K,V> with possible duplicate Keys to a LinkedHashMap<K,V> sorted according the Comparator 
-	public static<K,V> Map<K,V> convertHandlingDuplicatesToSortedMap(List<Map.Entry<K,V>> list, Comparator<Map.Entry<K,V>> comparator) {
+	public static<K,V> Map<K,V> convertHandlingDuplicatesToSortedMap(List<Map.Entry<K,V>> list,
+																		Comparator<? super Map.Entry<K,V>> comparator) {
 		if(list==null)
 			return null;
 	  
